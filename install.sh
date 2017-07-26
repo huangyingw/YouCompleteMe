c248d904f1d4a74c912e479bbb27d70ebcea5ed2
Ying Huang <yhuang@imvu.com>
2017-07-25 21:25:21 -0700

    n

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+brew install go node npm mono
+brew install caskroom/cask/brew-cask
+brew cask install cargo
 git submodule update --init --recursive
 ./install.py --all
