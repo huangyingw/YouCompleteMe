ac2ac8c498ecdb2f94c4164f2ba8f5e8a6f9d900
Ying Huang <yhuang@imvu.com>
2017-12-20 15:38:14 -0800

    n

--- install.sh
+++ install.sh
@@ -6,11 +6,15 @@ cd "$SCRIPTPATH"
 OS=`uname`
 if [ $OS != "Darwin" ];
 then
+    . /etc/lsb-release
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/debian zesty main" |  tee /etc/apt/sources.list.d/mono-xamarin.list
+    echo "deb http://download.mono-project.com/repo/debian $DISTRIB_CODENAME main" |  tee /etc/apt/sources.list.d/mono-xamarin.list
     apt-get update
-    apt-get install -y python-dev mono-complete
+    apt-get install -y python-dev mono-complete golang-go
+    curl -sL https://deb.nodesource.com/setup | bash -
+    apt-get install -y nodejs npm
     npm install xbuild
+    curl -sSf https://static.rust-lang.org/rustup.sh | sh
 else
     brew install go node npm mono CMake
     brew install caskroom/cask/brew-cask
