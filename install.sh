80e0c740fc28092e76003987b74d9ed9b6eec9ab
Ying Huang <yhuang@imvu.com>
2018-01-22 18:11:32 -0800

    n

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-OS=`uname`
-if [ $OS != "Darwin" ]
+if [ $(uname) != "Darwin" ]
 then
     . /etc/lsb-release
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
